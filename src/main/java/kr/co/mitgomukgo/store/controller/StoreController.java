55af45feefb27fe66cd43e6cd13738487407ad9b
YNY <nyyang9921@gmail.com>
2022-10-26 20:17:54 +0900

    맛집 상세 페이지 이동

--- src/main/java/kr/co/mitgomukgo/store/controller/StoreController.java
+++ src/main/java/kr/co/mitgomukgo/store/controller/StoreController.java
@@ -2,6 +2,7 @@ package kr.co.mitgomukgo.store.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import kr.co.mitgomukgo.store.model.service.StoreService;
 
@@ -13,6 +14,12 @@ public class StoreController {
 
 	public StoreController() {
 		super();
+	}
+	
+	//맛집 상세 보기 페이지 이동
+	@RequestMapping(value="/storeDetailView.do")
+	public String storeDetailView() {
+		return "store/storeDetail";
 	}
 	
 }
---
+++ src/main/webapp/WEB-INF/views/store/storeDetail.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>맛집상세 페이지</title>
+</head>
+<body>
+<h3>잘나오니?</h3>
+</body>
+</html>
