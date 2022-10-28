cfdb1bbe07345e3205817fc0859d8a62efb0d0f5
YNY <nyyang9921@gmail.com>
2022-10-28 13:52:06 +0900

    커밋

--- src/main/java/kr/co/mitgomukgo/store/controller/StoreController.java
+++ src/main/java/kr/co/mitgomukgo/store/controller/StoreController.java
@@ -9,13 +9,11 @@ import kr.co.mitgomukgo.store.model.service.StoreService;
 @Controller
 public class StoreController {
 	
+	
+
 	@Autowired
 	private StoreService service;
 
-	public StoreController() {
-		super();
-	}
-	
 	//맛집 상세 보기 페이지 이동
 	@RequestMapping(value="/storeDetailView.do")
 	public String storeDetailView() {
