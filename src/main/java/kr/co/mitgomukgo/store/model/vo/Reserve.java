cfab5fa7a66c42456aab56e4f9f7945dd47cb329
YNY <nyyang9921@gmail.com>
2022-11-03 16:50:20 +0900

    예약 vo 생성

---
+++ src/main/java/kr/co/mitgomukgo/store/model/vo/Reserve.java
@@ -0,0 +1,22 @@
+package kr.co.mitgomukgo.store.model.vo;
+
+import java.util.ArrayList;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Reserve {
+
+	private int reserveNo;
+	private int memberNo;
+	private int storeNo;
+	private String storeName;
+	private String reserveDate;
+	private String eatDate;
+	private String eatTime;
+	
+}
