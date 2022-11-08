c6e0fb7f8765bfda89445700cd54fcfd2ed686eb
JWbase <jungwoo1002@gmail.com>
2022-11-08 13:38:25 +0900

    - 패키지 수정

--- src/main/java/kr/or/mitgomukgo/cart/controller/CartController.java
+++
@@ -1,9 +0,0 @@
-package kr.or.mitgomukgo.cart.controller;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-public class CartController {
-}
