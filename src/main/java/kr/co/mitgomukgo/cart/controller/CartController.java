5be216b93ac438cf0a1fc0b934b0d6cfc4839b38
Sanna <tntkssk1233@gmail.com>
2022-11-10 16:52:12 +0900

    커밋

--- src/main/java/kr/co/mitgomukgo/cart/controller/CartController.java
+++ src/main/java/kr/co/mitgomukgo/cart/controller/CartController.java
@@ -1,10 +1,15 @@
 package kr.co.mitgomukgo.cart.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import kr.co.mitgomukgo.cart.model.service.CartService;
+import kr.co.mitgomukgo.cart.model.vo.Cart;
+import kr.co.mitgomukgo.member.model.vo.Member;
 
 
 
@@ -12,9 +17,13 @@ import kr.co.mitgomukgo.cart.model.service.CartService;
 public class CartController {
 	@Autowired
 	private CartService service;
+
 	
 	@RequestMapping(value="/cart.do")
-	public String cart() {
-		return "/order/cart";
-	}
+	   public String cart(Model model, Member m) {
+		  ArrayList<Cart> list = service.allCartList(m);
+	      model.addAttribute("list",list);
+	      return "market/cart";
+	   }
+	
 }
--- src/main/java/kr/co/mitgomukgo/cart/model/dao/CartDao.java
+++ src/main/java/kr/co/mitgomukgo/cart/model/dao/CartDao.java
@@ -1,8 +1,22 @@
 package kr.co.mitgomukgo.cart.model.dao;
 
+import java.util.ArrayList;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import kr.co.mitgomukgo.cart.model.vo.Cart;
+import kr.co.mitgomukgo.member.model.vo.Member;
+import java.util.List;
 
 @Repository
 public class CartDao {
-
+	
+	@Autowired
+	private SqlSessionTemplate sqlSession; 
+	public ArrayList<Cart> allCartList(Member m) {
+		 List list = sqlSession.selectList("cart.allCartList", m);
+	      return (ArrayList<Cart>) list;
+	   }
 }
